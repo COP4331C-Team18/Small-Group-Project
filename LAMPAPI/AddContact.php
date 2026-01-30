ed1daa276551b88e80f1faf27eb6be33b7f3d1d3
Akin Korkmaz <akinkorkmaz13@gmail.com>
2026-01-30 17:39:49 -0500

    Add Contacts Tested with Postman {     "Firstname": "Akin",     "Lastname": "Korkmaz",     "Email": "akin@example.com",     "Phone": "321-555-1234",     "UserID": 1 }

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,67 @@
+<?php
+
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    $inData = getRequestInfo();
+
+    $firstName = $inData['Firstname'];
+    $lastName = $inData['Lastname'];
+    $email = $inData['Email'];
+    $phone = $inData['Phone'];
+    $userId = $inData['UserID'];
+
+    // Connect to the database
+    $conn = new mysqli("localhost", username: "TheBeast", "WeLoveCOP4331", "NEBULIST");
+
+    if ($conn->connect_error) 
+    {
+        returnWithError($conn->connect_error);
+    } 
+    else 
+    {
+        //inserts into table Users
+        $stmt = $conn->prepare("INSERT INTO Contacts (Firstname, Lastname, Email, Phone, UserID) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $userId);
+        
+        
+        if ($stmt->execute()) 
+        {
+            returnWithInfo("Registered successfully");
+        } 
+        else 
+        {
+            // username?/email?/phone? exists in Database
+            returnWithError($stmt->error);
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    // Helpers from Login PHP
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    //Edited for sending msg
+    function returnWithInfo($msg)
+    {
+        $retValue = '{"message":"' . $msg . '", "error":""}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
